5f532982a1db04d4b0e3bf95ee644039e1aabadb
Johnny Behague <johnny.behague@live.fr>
2018-03-21 19:55:48 +0100

    add first version

---
+++ EventBus/EventBus/Abstractions/IDynamicIntegrationEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace LilaSoft.Patterns.EventBus.EventBus.Abstractions
+{
+    using System.Threading.Tasks;
+
+    public interface IDynamicIntegrationEventHandler
+    {
+        Task Handle(dynamic eventData);
+    }
+}
---
+++ EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using LilaSoft.Patterns.EventBus.EventBus.Events;
+using System.Threading.Tasks;
+
+namespace LilaSoft.Patterns.EventBus.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
